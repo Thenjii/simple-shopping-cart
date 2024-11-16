59f44f0d3fd1d062ad25a1a1ec15fd6037fc7011
Thenjiwe <thenjiwelamani15@gmail.com>
2024-11-16 23:10:46 +0200

    cart

--- script.js
+++ script.js
@@ -116,10 +116,39 @@ function totalCost(product){
 function displayCart(){
     let cartItems = localStorage.getItem("productsInCart");
     cartItems =JSON.parse(cartItems);
-    let productContainer = document.querySelector(".product-container");
+    let productContainer = document.querySelector(".products");
     if(cartItems && productContainer){
         productContainer.innerHTML= '';
-        
+        Object.values(cartItems).map(item=> {
+            productContainer.innerHTML+= `
+            <div class ="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                <img src = "./images.${item.tag}.jpg">
+                <span>${item.name}</span>
+                </div>
+                <div class="price">
+                    $${item.price},00
+                </div>
+                <div class="quantity">
+                    <ion-icon name="arrow-back-circle-outline"></ion-icon>
+                    <span>${item.inCart}</span>
+                    <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+                </div>
+                <div class="total">
+                   $${item.inCart *item.price},00
+                </div>
+            `
+        });
+
+        productContainer.innerHTML +=`
+            <div class ="basketTotalContainer">
+                <h4 class="cartTotalTittle">
+                    Cart Total
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost},00;
+        `
+
     }
 }
 
